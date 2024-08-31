4cdf837038f8f9e47402cb1116766343d2be5922
KunKimlong <kimlongkun2412@gmail.com>
2024-08-31 12:56:08 +0700

    new

---
+++ src/main/java/org/learning/firstproject/model/request/StudentRequest.java
@@ -0,0 +1,4 @@
+package org.learning.firstproject.model.request;
+
+public class StudentRequest {
+}
---
+++ src/main/java/org/learning/firstproject/model/response/StudentResponse.java
@@ -0,0 +1,4 @@
+package org.learning.firstproject.model.response;
+
+public class StudentResponse {
+}
